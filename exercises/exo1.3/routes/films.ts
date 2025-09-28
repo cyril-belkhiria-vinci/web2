2fea10330721ab2a6f0e96ccc98d1fac2456ed90
Cyril Belkhiria <cyril.belkhiria@student.vinci.be>
2025-09-28 19:16:30 +0200

    new ex1.3

--- exercises/exo1.3/routes/films.ts
+++ exercises/exo1.3/routes/films.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { Film } from "../types";
 
 const router = Router();
-let nbrAPI = 0 ;
 const defaultFilms : Film[] = [
     {
         id:1,
@@ -26,12 +25,31 @@ const defaultFilms : Film[] = [
 
 router.get("/",(req,res)=>{
     if (req.query.order && typeof req.query.order !== "string") {
-    return res.sendStatus(400);
+        return res.status(400);
   }
-    nbrAPI = nbrAPI + 1 ;
     console.log(req.path);
-    console.log("GET COUNTER : " + nbrAPI)
-    return res.json(defaultFilms)
-})
+    const mindur = req.query['minimum-duration'];
+    if(mindur === undefined)
+        return res.status(200).json(defaultFilms);
+    const min = Number(mindur);
+    if(Number.isNaN(min))
+        return res.status(400).json("Wrong minimum duration");
+    const filtre = defaultFilms.filter(f=>Number(f.duration)>=min);
+    return res.status(200).json(filtre);
+    
+});
+
+router.get("/:id",(req,res)=>{
+    if (req.query.order && typeof req.query.order !== "string") {
+    return res.status(400);
+  }
+  const id = Number(req.params.id);
+  if(!isNaN(id))
+    return res.status(400).json("Id non valide");
+  const film = defaultFilms.find(film=>film.id === id);
+  if(!film)
+    return res.status(404).json("This film doesn't exist");
+  return res.status(200).json(film);
+});
 
 export default router;
