616608574bbf67a5b7dd41642cc2fcff43afd5d9
Cyril Belkhiria <cyril.belkhiria@student.vinci.be>
2025-10-15 12:03:03 +0200

    new photo ex2.4

--- exercises/exo2.1/Movies/src/App.tsx
+++ exercises/exo2.1/Movies/src/App.tsx
@@ -5,9 +5,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => {
-  const headerLogoUrl ="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=100&q=80";
+  const headerLogoUrl ="https://plus.unsplash.com/premium_photo-1672116453000-c31b150f48ef?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170";
 
-  const footerLogoUrl ="https://images.unsplash.com/photo-1500534623283-312aade485b7?auto=format&fit=crop&w=100&q=80";
+  const footerLogoUrl ="https://images.unsplash.com/photo-1532993680872-98b088e2cacd?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170";
 
   const pageTitle = "Informations sur les films dans les cinémas";
 
