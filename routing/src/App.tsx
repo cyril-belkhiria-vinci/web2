a7d22ef3f8490a1c7d5581637aa9f23877ff3cdb
Cyril Belkhiria <cyril.belkhiria@student.vinci.be>
2025-11-05 11:18:55 +0100

    théorie router

---
+++ routing/src/App.tsx
@@ -0,0 +1,28 @@
+import { Outlet, useNavigate } from "react-router-dom";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  return (
+    <nav>
+      <button onClick={() => navigate("/")}>Home</button>
+      <button onClick={() => navigate("/about")}>About</button>
+      <button onClick={() => navigate("/contact")}> Contact</button>
+    </nav>
+  );
+};
+
+const HomePage = () => <p>Home Page</p>;
+
+const AboutPage = () => <p>About Page</p>;
+
+const ContactPage = () => <p>Contact Page</p>;
+
+const App = () => (
+  <div>
+    <NavBar />
+    <Outlet />
+  </div>
+);
+
+export default App;
+export { HomePage, AboutPage, ContactPage };
